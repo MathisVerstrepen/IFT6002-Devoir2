889756a973722410a482d62bff0228c5b1a85e39
mathis <mathious61@gmail.com>
2024-09-16 20:22:31 -0400

    first step

--- src/test/java/ca/ulaval/glo4002/mockexercise/StartByTestingThisTest.java
+++ src/test/java/ca/ulaval/glo4002/mockexercise/StartByTestingThisTest.java
@@ -1,13 +1,50 @@
 package ca.ulaval.glo4002.mockexercise;
 
+import ca.ulaval.glo4002.mockexercise.do_not_edit.CartFactory;
+import ca.ulaval.glo4002.mockexercise.do_not_edit.ProductRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+import static org.mockito.Mockito.mock;
 
 public class StartByTestingThisTest {
-
+    private CartFactory cartFactoryMock;
+    private ProductRepository productRepositoryMock;
     private StartByTestingThis service;
+    private Cart cartMock;
+    private final Product product = new Product("0", "0", 10);
 
     @BeforeEach
     public void setUp() {
-        service = new StartByTestingThis();
+        cartFactoryMock = mock(CartFactory.class);
+        productRepositoryMock = mock(ProductRepository.class);
+
+        cartMock = mock(Cart.class);
+        when(cartFactoryMock.create(anyString())).thenReturn(cartMock);
+        when(productRepositoryMock.findBySku(anyString())).thenReturn(product);
+
+        service = new StartByTestingThis(cartFactoryMock, productRepositoryMock);
+    }
+
+
+    @Test
+    public void whenOneClickBuyUsed_thenCartFactoryIsCalled() {
+        service.oneClickBuy("test2", "0");
+        verify(cartFactoryMock, times(1)).create("test2");
+    }
+
+    @Test
+    public void whenOnClickBuyUsed_thenProductRepositoryIsCalled() {
+        service.oneClickBuy("test", "0");
+        verify(productRepositoryMock, times(1)).findBySku("0");
+    }
+
+    @Test
+    public void whenOneClickBuyUsed_thenCartAddIsCalled() {
+        service.oneClickBuy("test", "0");
+
+        verify(cartMock, times(1)).addProduct(product);
     }
 }
